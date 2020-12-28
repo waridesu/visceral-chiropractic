bde6f4503f0cee6a64909ac91426b041f39bccdc
Denis <waridesu@gmail.com>
2020-12-28 17:21:09 +0200

    fotter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import './App.css';
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import CarouselIntro from "./Components/Carousel/Carousel";
--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,13 +1,54 @@
 import React from "react";
-import {Container} from "react-bootstrap";
+import {Col, Container} from "react-bootstrap";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faVk} from '@fortawesome/free-brands-svg-icons'
+import {faFacebook} from '@fortawesome/free-brands-svg-icons'
+import {faInstagram} from '@fortawesome/free-brands-svg-icons'
+
 
 function Footer() {
     return <footer className="bg-light mt-auto">
-        <Container className="d-grid d-sm-flex justify-content-center justify-content-sm-between">
-        <span>Телефон</span>
-        <span>Соцсети</span>
-        <span>Место</span>
+        <Container>
+            <div className='d-flex flex-column flex-md-row'>
+                <Col className="text-center">
+                    <h6>Телефон</h6>
+                    <a className="text-decoration-none" href="tel:+380 067 605 93 37">
+                        +380 067 605 93 37
+                    </a><br/>
+                    <a className="text-decoration-none" href="tel:+380 067 605 93 37">
+                        +380 067 605 93 37
+                    </a><br/>
+                    <a className="text-decoration-none" href="tel:+380 067 605 93 37">
+                        +380 067 605 93 37
+                    </a>
+                </Col>
+                <Col className="text-center">
+                    <h6>Соцсети</h6>
+                    <div className="d-flex justify-content-evenly">
+                        <a href="Fb" className="">
+                            <FontAwesomeIcon icon={faFacebook}/>
+
+                        </a>
+                        <a href="vk" className="fa fa">
+                            <FontAwesomeIcon icon={faVk}/>
+                        </a>
+                        <a href="In" className="fa fa">
+                            <FontAwesomeIcon icon={faInstagram}/>
+                        </a>
+                    </div>
+                </Col>
+                <Col className="text-center">
+                    <h6>Место</h6>
+                </Col>
+            </div>
+            <div className="row before-copyright">
+                <p className="col-sm text-center">
+                    &copy; 2020 | All right reserved | Terms of Service | Privacy
+                </p>
+            </div>
         </Container>
+
+
     </footer>
 }
 
